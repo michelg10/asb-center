eec00f2fa046161710a618adc12027f1f61c69af
Michel <legitmichel777@outlook.com>
2022-06-01 16:02:27 +0800

    Finished Guess the baby

---
+++ cloudfunctions/GuessTheBabySubmit/index.js
@@ -0,0 +1,46 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    const wxContext = cloud.getWXContext();
+
+    let db = cloud.database();
+    let closingTime = await db.collection("BabyTeachersInfo").doc("limitTime").get();
+    const currentTime = Date.now()/1000.0;
+    if (currentTime>closingTime.data.value) {
+        return {};
+    }
+    let getUserId = await db.collection("userData").where({
+        userId: wxContext.OPENID,
+    }).get();
+    if (getUserId.data.length>1 || getUserId.data.length === 0) {
+        console.log("userId "+wxContext.OPENID+" has unexpected "+getUserId.length+" accounts");
+        return;
+    }
+
+    let userId = getUserId.data[0]._id;
+    let newGuessTheBabyData = {
+        choices: event.userResponses,
+        correspondingOpenId: wxContext.OPENID,
+        userId: userId
+    }
+
+    let getExistingId = await db.collection("BabyTeachersData").get({
+        userId: userId,
+    });
+    if (getExistingId.data.length === 0) {
+        db.collection("BabyTeachersData").add({
+            data: newGuessTheBabyData
+        });
+    } else {
+        const existingId = getExistingId.data[0]._id;
+        db.collection("BabyTeachersData").doc(existingId).set({
+            data: newGuessTheBabyData,
+        });
+    }
+
+    return { }
+}
--- pages/GuessTheBaby/GuessTheBaby.ts
+++ pages/GuessTheBaby/GuessTheBaby.ts
@@ -20,6 +20,7 @@ interface ComponentDataInterface {
     userId: string,
     questionDisplayInformation: QuestionDisplayInformation[],
     isActive: boolean,
+    serverFetchedChoices: number[],
 }
 
 Component({
@@ -47,11 +48,19 @@ Component({
                     newQuestionDisplayInformation[i].imageDisplayUrl = imageUrls[i];
                 }
             }
+            if (this.data.response !== undefined && this.data.teacherNames !== undefined) {
+                for (let i=0;i<this.data.babyQuestionCount;i++) {
+                    newQuestionDisplayInformation[i].response = this.data.teacherNames[this.data.response[i]];
+                }
+            }
             this.setData({
                 questionDisplayInformation: newQuestionDisplayInformation,
             });
         },
         responseTapped: function(x: any) {
+            if (this.data.isActive === false) {
+                return;
+            }
             console.log("Response tapped");
             let index = x.currentTarget.dataset.index;
             this.setData({
@@ -66,6 +75,16 @@ Component({
             this.setData({
                 currentActiveDropdown: -1,
                 response: newData,
+            });
+            this.buildDisplayInformation();
+            this.uploadData();
+        },
+        uploadData: function() {
+            wx.cloud.callFunction({
+                name: "GuessTheBabySubmit",
+                data: {
+                    userResponses: this.data.response,
+                }
             });
         },
         dismissDropdown: function() {
@@ -84,7 +103,24 @@ Component({
                 this.setData({
                     teacherNames: teacherNames,
                 });
+                this.buildDisplayInformation();
             })
+        },
+        mergeServerData: function() {
+            console.log("MERGE");
+            console.log(this.data.response);
+            console.log(this.data.serverFetchedChoices);
+            if (this.data.response === undefined || this.data.serverFetchedChoices === undefined) {
+                return;
+            }
+            let newData = this.data.response;
+            for (let i=0;i<newData.length;i++) {
+                if (this.data.serverFetchedChoices[i] !== null && this.data.serverFetchedChoices[i] !== undefined) {
+                    newData[i] = this.data.serverFetchedChoices[i];
+                }
+            }
+            this.data.response = newData;
+            this.buildDisplayInformation();
         },
         onLoad: function() {
             const eventChannel = this.getOpenerEventChannel();
@@ -98,7 +134,7 @@ Component({
                 });
             })
             this.data.db.collection("BabyTeachersInfo").doc("limitTime").get().then((res) => {
-                let closingTime = res.data.data;
+                let closingTime = res.data.value;
                 if (Date.now()/1000.0<=closingTime) {
                     this.setData({
                         isActive: true,
@@ -109,12 +145,24 @@ Component({
                     });
                 }
             })
+            eventChannel.on("userId", (data: any) => {
+                this.data.userId = data;
+                this.data.db.collection("BabyTeachersData").where({
+                    correspondingOpenId: "{openid}"
+                }).get().then((res) => {
+                    if (res.data.length === 1) {
+                        this.data.serverFetchedChoices = res.data[0].choices;
+                        this.mergeServerData();
+                    }
+                })
+            })
             eventChannel.on('cacheSingleton', async (data: any) => {
                 this.data.cacheSingleton = data;
                 // fetch count
                 let totalSize = await this.data.db.collection("BabyTeachersInfo").doc("size").get();
                 this.data.questionDisplayInformation = Array<QuestionDisplayInformation>(totalSize.data.value as number);
                 this.data.response = Array<number>(totalSize.data.value as number);
+                this.mergeServerData();
                 this.setData({
                     babyQuestionCount: totalSize.data.value,
                 });
