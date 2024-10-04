a031e1cb65d140c9e703e68ed9640f65511b49b8
Michael Yu <115764282+MichaelY0222@users.noreply.github.com>
2024-10-04 21:47:56 +0800

    Updated Mini Program for SM24
    Updated e-stamp design and layout for Sports Meet 2024.

---
+++ classes/gNumber.ts
@@ -0,0 +1,10 @@
+export class gNumber {
+  id: string;
+  gnumber: string;
+  studentId: string;
+  constructor(id: string, gnumber: string, studentId: string) {
+    this.id = id;
+    this.gnumber = gnumber;
+    this.studentId = studentId;
+  }
+}
